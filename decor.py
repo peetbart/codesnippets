d19e0a7af9845f1592f787432a076e0f07279b8d
danf@VBox-Ubu19.04 <daniel.forgacs@burrows.yr.com>
2019-06-07 15:48:40 +0100

    added simple decorator example

--- decor.py
+++ decor.py
@@ -3,8 +3,20 @@ example for decorators.
 """
 
 
+def decor(func):
+    def wrapper():
+        print '::before'
+        result = func()
+        print '::after'
+        return result
+
+    return wrapper
+
+
+@decor
 def base_func():
     print '-base func running-'
+
 
 
 
