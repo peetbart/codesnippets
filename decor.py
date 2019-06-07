179ef5a20dfd78adadadd9dbe0f82a1532192bf5
danf@VBox-Ubu19.04 <daniel.forgacs@burrows.yr.com>
2019-06-07 16:11:44 +0100

    added docs

--- decor.py
+++ decor.py
@@ -1,22 +1,36 @@
 def extra(func):
-    print '-extra-'
+    """
+    This is the function that returns the new function
+    that wraps your original function and calls it
+    when you make a call with the original function's name
+    your original function is an argument "func" & gets called
+    when this function is called, which happend when the
+    original name is called.
+    """
 
     def caller():
-        func()
+        # Put extra code BEFORE you run you original func
+        print '-extra before-'
+        result = func()
+        print '-extra after-'
+        # Put extra code AFTER you run you original func
 
     return caller
 
 
+# This syntax: "@"; the decorator is just a syntactic
+# sugar to do the same as the variable assignment below
+@extra
 def base_func():
     """original func you don't / can't modify """
     print '-base func running-'
 
 
 
-# you put your original function call in another
-# function, so you can leave the original func
-# and calling it untouched.
-base_func = extra(base_func)
+# # you put your original function call in another
+# # function, so you can leave the original func
+# # and calling it untouched.
+# base_func = extra(base_func)
 
 
 # Original func call you can't / dont't want to modufy:
