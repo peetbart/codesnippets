561b653a62a2dd8e160d7d7085588a57d006943a
danf@VBox-Ubu19.04 <daniel.forgacs@burrows.yr.com>
2019-06-07 16:00:54 +0100

    next step

--- decor.py
+++ decor.py
@@ -1,11 +1,16 @@
-def extra():
+def extra(func):
     print '-extra-'
+
+    def caller():
+        func()
+
+    return caller
 
 
 def base_func():
     print '-base func running-'
 
-base_func = extra
+base_func = extra(base_func)
 
 
 base_func()
