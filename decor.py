f02f3590f0079aa97d550b657f2ffcf1dd209290
danf@VBox-Ubu19.04 <daniel.forgacs@burrows.yr.com>
2019-06-07 16:02:27 +0100

    added docs

--- decor.py
+++ decor.py
@@ -8,9 +8,13 @@ def extra(func):
 
 
 def base_func():
+    """original func you don't / can't modify """
     print '-base func running-'
+
+
 
 base_func = extra(base_func)
 
 
+# Original func call you can't / dont't want to modufy:
 base_func()
